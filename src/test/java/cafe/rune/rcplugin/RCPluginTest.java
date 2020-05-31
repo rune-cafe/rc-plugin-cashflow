d7768724622e6ce527e861357b9f27fb3aa517b5
josh <jmhertlein@gmail.com>
2020-05-31 11:17:18 -0500

    fix rename

--- src/test/java/cafe/rune/rcplugin/RCPluginTest.java
+++ src/test/java/cafe/rune/rcplugin/RCPluginTest.java
@@ -3,7 +3,7 @@ package cafe.rune.rcplugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class CashFlowPluginTest
+public class RCPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
